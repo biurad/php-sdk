11629b106b8179f0afba8365139d9e6b554fc73f
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-10-23 14:18:22 +0000

    Updated container loader for nette di

--- src/ContainerLoader.php
+++ src/ContainerLoader.php
@@ -142,6 +142,26 @@ class ContainerLoader extends Configurator
     }
 
     /**
+     * @return array<string,mixed>
+     */
+    protected function getDefaultParameters(): array
+    {
+        $trace     = \debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS);
+        $last      = \end($trace);
+        $debugMode = static::detectDebugMode();
+        $loaderRc  = \class_exists(ClassLoader::class) ? new ReflectionClass(ClassLoader::class) : null;
+
+        return [
+            'appDir'         => isset($trace[2]['file']) ? \dirname($trace[2]['file']) : null,
+            'wwwDir'         => isset($last['file']) ? \dirname($last['file']) : null,
+            'vendorDir'      => $loaderRc ? \dirname($loaderRc->getFileName(), 2) : null,
+            'debugMode'      => $debugMode,
+            'productionMode' => !$debugMode,
+            'consoleMode'    => \PHP_SAPI === 'cli',
+        ];
+    }
+
+    /**
      * Get the loader
      */
     protected function createLoader(): DI\Config\Loader
