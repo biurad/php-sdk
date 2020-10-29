9aec7d8b716a89a5637988c0c023db7c3e7dd939
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-10-29 19:16:35 +0000

    Replaced router class with events route listener class

---
+++ src/Listeners/EventRouteListener.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Biurad opensource projects.
+ *
+ * PHP version 7.2 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Biurad\Framework\Listeners;
+
+use Biurad\Framework\Event\ControllerArgumentsEvent;
+use Biurad\Framework\Event\ControllerEvent;
+use Biurad\Framework\Interfaces\HttpKernelInterface;
+use Biurad\Framework\KernelEvents;
+use Flight\Routing\Interfaces\RouteInterface;
+use Flight\Routing\Interfaces\RouteListenerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class EventRouteListener implements RouteListenerInterface
+{
+    /** @var EventDispatcherInterface */
+    private $dispatcher;
+
+    /** @var HttpKernelInterface */
+    private $kernel;
+
+    /**
+     * @param EventDispatcherInterface $dispatcher
+     */
+    public function __construct(EventDispatcherInterface $dispatcher, HttpKernelInterface $kernel)
+    {
+        $this->kernel     = $kernel;
+        $this->dispatcher = $dispatcher;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onRoute(ServerRequestInterface $request, RouteInterface &$route, callable $callable): void
+    {
+        if ('phpinfo' === $callable) {
+            $route->setArguments(['what' => -1]);
+        }
+
+        $event = new ControllerEvent($this->kernel, $callable, $request);
+        $this->dispatcher->dispatch($event, KernelEvents::CONTROLLER);
+
+        $event = new ControllerArgumentsEvent(
+            $this->kernel,
+            $event->getController(),
+            $route->getArguments(),
+            $request
+        );
+        $this->dispatcher->dispatch($event, KernelEvents::CONTROLLER_ARGUMENTS);
+    }
+}
